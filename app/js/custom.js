ef612ae40a850471ce8d393fb78d73ffaf6f1f03
Khoteev N.D <khoteevnd@gmail.com>
2017-09-12 17:41:43 +0300

    bg add image

--- app/js/custom.js
+++ app/js/custom.js
@@ -108,53 +108,6 @@ $(window).on('load', function () {
 	$preloader.delay(350).fadeOut('slow');
 });
 
-//OWL-CAROUSEL SETTINGS
-$(document).ready(function(){
-$(".owl-carousel").owlCarousel({
-		animateOut: 'slideOutDown',
-		animateIn: 'flipInX',
-		autoWidth:true,
-		items:1,
-		center: true,
-		margin:30,
-		stagePadding:30,
-		smartSpeed:1000,
-		nav: true,
-		navText: [
-			"<i class=\"fa fa-3x fa-angle-left\" aria-hidden=\"true\"></i>",
-			"<i class=\"fa fa-3x fa-angle-right\" aria-hidden=\"true\"></i>"
-		],
-		dots: false
-	});
-});
-//OWL-CAROUSEL BTN Animate
-$(function(){
-	$('.owl-prev').hover(
-		function() {
-			$('.owl-prev')
-				.removeClass('opacityIn animated')
-				.addClass('opacityOut animated');
-		},
-		function() {
-			$('.owl-prev')
-				.removeClass('opacityOut animated')
-				.addClass('opacityIn animated');
-		}
-	);
-	$('.owl-next').hover(
-		function() {
-			$('.owl-next')
-				.removeClass('opacityIn animated')
-				.addClass('opacityOut animated');
-		},
-		function() {
-			$('.owl-next')
-				.removeClass('opacityOut animated')
-				.addClass('opacityIn animated');
-		}
-	);
-});
-
 //MAP: Animate
 /*$(function(){
 	$('.map').hover(
@@ -176,14 +129,3 @@ $(function(){
 		}
 	);
 });*/
-
-/*
- function myresize() {
-		var w = window.innerWidth;
-		var h = window.innerHeight;
-		document.getElementById("header").style.width = w - document.getElementById("nav").style.width + "px";
-		document.getElementById("header").style.height = h - document.getElementById("nav").style.height + "px";
-		//console.log(w + " " + h);
-}
- document.addEventListener("DOMContentLoaded", mysize);
- */
